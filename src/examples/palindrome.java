b846f89e743d860c0a908a8ff718f807d3dacc04
Gerardo Aponte <ing.aponte@gmail.com>
2016-08-20 13:55:12 -0400

    Add more code

--- src/examples/palindrome.java
+++ src/examples/palindrome.java
@@ -1,18 +1,45 @@
 package examples;
 
+
 import java.util.Scanner;
+
+
 
 public class palindrome {
 
-	private static Scanner s;
+	private static Scanner   s;
 	
 	public static void main(String[] args) {
-    s = new Scanner(System.in);
-    System.out.print("Enter any number value ");
+		
+		
+	s = new Scanner(System.in);
+	
+	 do{
+    System.out.print("Enter a valid number value ");
+    boolean isnum;
+
+    do{
+    if(s.hasNextInt())
+    {
+    
     int n = s.nextInt();
     npalindrome(n);
     
-}
+    isnum = true;
+    }else 
+    {
+    	System.out.print("This is not a number, please enter a valid number ");
+    	isnum = false;
+    	s.next();
+    	
+    }
+    }while(!(isnum));
+
+        System.out.println("You want to continue : (Y/N) ");
+    } while("Y".equalsIgnoreCase(s.next().trim()));
+	}
+	
+	
 	 public static void npalindrome(int n) {
 	int num =0; 
 	num = n;
--- src/examples/prime.java
+++ src/examples/prime.java
@@ -6,12 +6,34 @@ public class prime {
 	
 	private static Scanner s;
 	public static void main(String[] args) {
+		
+		do{
 	       s = new Scanner(System.in);
-	       System.out.print("Enter a Prime: ");
+	       System.out.print("Enter a Number: ");
+	       boolean isnum;
+	       do{
+	    	   if(s.hasNextInt())
+	    	    {
 	       float n = s.nextInt();
+	       isnum = true;
 	       nprime(n);
-	   }
-	 public static void nprime(float n) {
+	    	    } else 
+	    	    {
+	    	    	System.out.print("This is not a number, please enter a valid number ");
+	    	    	isnum = false;
+	    	    	s.next();
+	    	    }
+	    	   
+	   }while(!(isnum));
+	       System.out.println("You want to continue : (Y/N) ");
+	       
+    } while("Y".equalsIgnoreCase(s.next().trim()));
+	}
+	
+	 
+	
+	
+	public static void nprime(float n) {
 		 
 		 if (n <= 1 )
 		 {
@@ -33,7 +55,6 @@ public class prime {
 		 }
 		 return;
 		 }
-		
 	 
 	 }
 
